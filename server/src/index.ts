107116fc507459856867f9354ea0959145574336
danny <danixeka@gmail.com>
2020-04-11 09:28:39 +1000

    restarted app and added original files

--- server/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { ApolloServer } from "apollo-server";
-
-import typeDefs from "./schema";
-
-const server = new ApolloServer({ typeDefs });
-
-server.listen().then(res => console.log(`Server is ready at ${res.url}`));
-
-console.log('hello world');
